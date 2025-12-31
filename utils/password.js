97486f84ecd505652ba26f460026712a9e049fdd
Dhaval2203 <parekhdhaval1998@gmail.com>
2025-12-31 22:35:39 +0530

    Added function

--- utils/password.js
+++ utils/password.js
@@ -1,5 +1,10 @@
 import bcrypt from 'bcryptjs';
 
-export const comparePassword = async (plain, hashed) => {
-    return await bcrypt.compare(plain, hashed);
+export const hashPassword = async (password) => {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+    return bcrypt.compare(password, hashedPassword);
 };
